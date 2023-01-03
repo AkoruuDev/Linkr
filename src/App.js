f39c31512edf0f2c49e7347916ab6abdf303cdd9
jojobaa <joycarvalho59@gmail.com>
2023-01-03 19:06:30 -0300

    first version of sing up page layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 //import logo from './logo.svg';
-//import SignUp from "./SignUp";
+import { Reset } from "./assets/Reset";
+import SignUp from "./pages/SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Reset />
       <Routes>
-        {/* <Route path="/" element={<SignIn />} />
-        <Route path="/cadastre" element={<SignUp />} /> */}
+        {/* <Route path="/" element={<SignIn />} /> */}
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
