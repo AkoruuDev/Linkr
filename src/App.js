27ce962a63d672de134d59c1a0702a1020f53a18
Akoruu.Dev <akoruu.dev@gmail.com>
2023-01-03 22:36:38 -0300

    STYLE [globalStyle]: Create GlobalStyle function and connect on main file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 //import logo from './logo.svg';
 // import SignUp from "./pages/log/SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/globalStyles";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<SignIn />} />
-        <Route path="/cadastre" element={<SignUp />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      < GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<SignIn />} />
+          <Route path="/cadastre" element={<SignUp />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
