99821e199194afcf0739e8bae966b6778ccb49e8
Akoruu.Dev <akoruu.dev@gmail.com>
2023-01-08 12:34:01 -0300

    REFACTOR [Reset]: Delete reset.js from styles; delete infos folder files; remove comments from app main file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-//import logo from './logo.svg';
-import { Reset } from "./assets/Reset";
 import SignUp from "./pages/SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./styles/globalStyles";
@@ -12,13 +10,9 @@ export default function App () {
   return (
   <>
     <BrowserRouter>
-      <Reset />
       < GlobalStyle />
         <Routes>
-          {/* <Route path="/" element={<SignIn />} />
-          <Route path="/cadastre" element={<SignUp />} /> */}
           <Route path="/" element={<SignUp />} />
-
           <Route path='/user/:id' element={< Profile />} />
           <Route path='/user/me' element={< MyProfile />} />
           <Route path='/hashtag/:hashtag' element={<Hashtag />} />
